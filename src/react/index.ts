ae5aaeaf738f1543e368dcf8c011923b67e6585e
Bjørge Næss <bjoerge@gmail.com>
2023-07-20 16:46:04 +0200

    fix: export events and react entry points

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,3 +1,4 @@
 export {useTelemetry} from './useTelemetry'
 export {TelemetryProvider} from './TelemetryProvider'
 export type {TelemetryContextValue} from './TelemetryProvider'
+export type * from '../types'
