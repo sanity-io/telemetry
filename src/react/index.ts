beece43c7cb7015e19a4571644ee33a1f3a1a389
Bjørge Næss <bjoerge@gmail.com>
2023-07-27 15:27:52 +0200

    fix: expose useTelemetryStoreLifeCycleEvents

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,3 +1,4 @@
 export {useTelemetry} from './useTelemetry'
+export {useTelemetryStoreLifeCycleEvents} from './useTelemetryStoreLifeCycleEvents'
 export {TelemetryProvider} from './TelemetryProvider'
 export type * from '../types'
