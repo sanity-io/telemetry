d90ba2a2e714db1bae86f80b02cc7b24f457a2c1
Bjørge Næss <bjoerge@gmail.com>
2023-11-22 16:14:04 +0100

    chore: remove ./events/manage directory

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,4 @@
 export * from './studio'
-export * from './manage'
 export * from './example'
 export * from './cli'
 export type * from '../types'
--- src/events/manage/index.ts
+++
@@ -1,8 +0,0 @@
-import {_defineLogEvent} from '../../internal'
-
-export const CorsOriginAdded = _defineLogEvent({
-  name: 'CorsOriginAdded',
-  displayName: 'Cors origin added',
-  description: 'Cors origin added to project',
-  version: 1,
-})
