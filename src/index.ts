dbb84879f859c46dc01711fceb3d36dbdc4409c6
Bjørge Næss <bjoerge@gmail.com>
2023-11-23 16:24:04 +0100

    fix: export _define(Trace|Log)Event

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ export * from './createSessionId'
 export * from './createBatchedStore'
 export * from './types'
 export * from './utils/setupLifeCycleListeners'
+
+export {_devDefineLogEvent, _devDefineTraceEvent} from './internal'
+export type {
+  DevelopmentTelemetryEvent,
+  DevelopmentTelemetryTrace,
+} from './internal'
