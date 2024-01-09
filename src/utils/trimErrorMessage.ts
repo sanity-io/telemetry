dc95df6c2f42d94c42d1020f7081a8f266ed113a
Bjørge Næss <bjoerge@gmail.com>
2024-01-09 11:00:17 +0100

    fixup! fix(store): cap error messages at first newline or when exceeding a char limit

--- src/utils/trimErrorMessage.ts
+++ src/utils/trimErrorMessage.ts
@@ -2,7 +2,7 @@ function splitAt(str: string, index: number) {
   return index < 1 ? [str, ''] : [str.substring(0, index), str.substring(index)]
 }
 
-const MAX_LENGTH = 100
+const MAX_LENGTH = 1024
 
 export function trimErrorMessage(errorMessage: string) {
   const newLineIndex = errorMessage.indexOf('\n')
